a199e6a0a1e44f43fa73024ea1bc313e97369bf6
JStruk <strukjustin@gmail.com>
2022-06-23 14:39:53 -0400

    use .env for firebase config

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,6 +1,18 @@
 import firebase from 'firebase'
 import admin from "firebase-admin";
-import serviceAccount from '../../config/firebase.json'
+
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": FIREBASE_PROJECT_ID,
+    "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+    "private_key": FIREBASE_PRIVATE_KEY,
+    "client_email": FIREBASE_CLIENT_EMAIL,
+    "client_id": FIREBASE_CLIENT_ID,
+    "auth_uri": FIREBASE_AUTH_URI,
+    "token_uri": FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": FIREBASE_CLIENT_CERT_URL
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
