46122f803bac3ee9ac14acefd469a41ccdca8a4d
JStruk <strukjustin@gmail.com>
2022-06-23 11:37:46 -0400

    send id back when fetching a list

--- src/routes/list.js
+++ src/routes/list.js
@@ -8,7 +8,7 @@ export const listRouter = express.Router();
 listRouter.get('/list', async (req, res) => {
     try {
         const list = await db.collection('lists').doc('/' + req.query.listId + '/').get()
-        return res.status(200).send(list.data());
+        return res.status(200).send({ id: list.id, ...list.data() });
     } catch (error) {
         console.log(error);
         return res.status(500).send(error);
