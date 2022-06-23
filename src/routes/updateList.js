6d0a4f0ff8dc27cb819716b2fa8d10a4158c03c7
JStruk <strukjustin@gmail.com>
2022-06-23 13:12:30 -0400

    allow user to update list title
    * Add new route `/updateList/title` to update list title
    * change firebase update method from `.set` -> `.update` so we don't overwrite the entire document

--- src/routes/updateList.js
+++ src/routes/updateList.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createOrUpdateListWithId } from "../utils/createOrUpdateListWithId.js";
+import { createOrUpdateListWithId, createOrUpdateListTitle } from "../utils/createOrUpdateListWithId.js";
 import { db } from "../database/firebase.js";
 
 export const updateListRouter = Router()
@@ -7,8 +7,18 @@ export const updateListRouter = Router()
 updateListRouter.post('/updateList', async (req, res) => {
     const listRef = await db.collection('lists').doc('/' + req.body.id + '/')
     const list = await listRef.get()
-    if(list.exists) {
+    if (list.exists) {
         res.status(204).send(await createOrUpdateListWithId(req.body.id, req.body.items))
+    }
+    res.status(404)
+})
+
+updateListRouter.post('/updateList/title', async (req, res) => {
+    console.log('id: ', req.body.id)
+    const listRef = await db.collection('lists').doc('/' + req.body.id + '/')
+    const list = await listRef.get()
+    if (list.exists) {
+        res.status(204).send(await createOrUpdateListTitle(req.body.id, req.body.title))
     }
     res.status(404)
 })
--- src/utils/createOrUpdateListWithId.js
+++ src/utils/createOrUpdateListWithId.js
@@ -1,7 +1,13 @@
 import { db } from "../database/firebase.js";
 
 export async function createOrUpdateListWithId(id, items) {
-    await db.collection('lists').doc('/' + id + '/').set({listItems: items});
+    await db.collection('lists').doc('/' + id + '/').update("listItems", items);
+    const updatedListRef = await db.collection('lists').doc(id)
+    return (await updatedListRef.get()).data()
+}
+
+export async function createOrUpdateListTitle(id, title) {
+    await db.collection('lists').doc('/' + id + '/').update("title", title);
     const updatedListRef = await db.collection('lists').doc(id)
     return (await updatedListRef.get()).data()
 }
